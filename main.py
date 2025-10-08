a816a5a61eb6edb6ddbdc941e5c67c2222b2f264
erik9691 <gagwaffle@gmail.com>
2025-10-08 20:55:52 -0300

    Definicion de listas paralelas

---
+++ main.py
@@ -0,0 +1,25 @@
+# Listas de productos
+productosId = []
+productosNombre = []
+productosCategoria = []
+productosPrecio = []
+productosStock = []
+productosVendidos = []
+productosRecaudacion = []
+
+# Listas de compras
+comprasId = []
+comprasDni = []
+comprasProductoId = []
+comprasCantidad = []
+comprasTotal = []
+comprasMedioPago = []
+
+# Listas de clientes
+clienteDni = []
+clienteRecaudacion = []
+clienteCompras = []
+
+# Listas de cupones
+cuponesCodigo = []
+cuponesDescuento = []
