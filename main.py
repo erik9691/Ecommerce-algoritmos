63e699391370625ad5ffb5d302a7b582090b32a1
erik9691 <gagwaffle@gmail.com>
2025-10-11 17:35:26 -0300

    Estadisticas

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Listas de productos
-productosId = []
-productosNombre = []
-productosCategoria = []
-productosPrecio = []
-productosStock = []
-productosVendidos = []
-productosRecaudacion = []
+productosId = ["id0", "id1", "id2", "id3"]
+productosNombre = ["Remera Algodón", "Pantalón Jean", "Consola PS5", "Auriculares BT"]
+productosCategoria = ["Vestimenta", "Vestimenta", "Tecnología", "Tecnología"]
+productosPrecio = [15.00, 45.00, 499.99, 75.50]
+productosStock = [50, 30, 10, 40]
+productosVendidos = [0, 0, 0, 0]
+productosRecaudacion = [0, 0, 0, 0]
 
 # Listas de compras
 comprasId = []
